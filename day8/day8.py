12f08ec09adcae601bb70d09dd7ed0c27d8a3058
mug1wara26 <aloysiusgsq@gmail.com>
2025-12-08 18:24:44 +0800

    Keep track of disjoint sets to improve time

--- day8/day8.py
+++ day8/day8.py
@@ -2,26 +2,17 @@ import heapq
 from itertools import combinations
 from ufds import DisjointSet
 import math
+import time
 
 
-class Pair:
-    def __init__(self, a, b) -> None:
-        self.a = a
-        self.b = b
-        self.distance = (
-            (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
-        ) ** 0.5
-
-    def __repr__(self) -> str:
-        return f"({self.a}, {self.b})"
-
-    def __lt__(self, other):
-        return self.distance < other.distance
+def distance(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2) ** 0.5
 
 
+start = time.time()
 data = list(
     map(
-        lambda x: Pair(x[0], x[1]),
+        lambda x: (distance(x[0], x[1]), x[0], x[1]),
         combinations(
             tuple(
                 map(
@@ -38,14 +29,18 @@ heapq.heapify(data)
 
 ds = DisjointSet()
 i = 0
+n = 1000
 
 while True:
     pair = heapq.heappop(data)
     i += 1
-    ds.union(pair.a, pair.b)
+    if ds.find(pair[1]) != ds.find(pair[2]):
+        n -= 1
+        ds.union(pair[1], pair[2])
     if i == 1000:
         print(f"Part 1: {math.prod(sorted(map(len, list(ds)), reverse=True)[:3])}")
 
-    if len(list(ds)[0]) == 1000:
-        print(f"Part 2: {pair.a[0] * pair.b[0]}")
+    if n == 1:
+        print(f"Part 2: {pair[1][0] * pair[2][0]}")
         break
+print(time.time() - start)
